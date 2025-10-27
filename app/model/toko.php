bb6cbb125163beb5875656c8393520351713e87e
Amelia7-supermassive <cameliamel.76@gmail.com>
2025-10-27 22:47:36 +0700

    Update toko.php

--- app/model/toko.php
+++ app/model/toko.php
@@ -3,6 +3,7 @@ class Toko {
     public function getData() {
         return [
             'nama' => 'EL-FATH FOTOCOPY',
+            'no.telp' => '081252477384',
             'alamat' => 'Jl. Veteran No. 51 Nganjuk'
         ];
     }
