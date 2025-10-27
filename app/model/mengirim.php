dbea0706765cd26c5ee9170d4dc1b6d2fa5c6a27
fadhilah31nf-star <fadhilah31.nf@gmail.com>
2025-10-27 22:22:05 +0700

    Update mengirim.php

--- app/model/mengirim.php
+++ app/model/mengirim.php
@@ -1,1 +1,21 @@
+<?php
+class Pengiriman_model extends CI_Model {
+
+    public function getAllPengiriman() {
+        return $this->db->get('pengiriman')->result();
+    }
+
+    public function insertPengiriman($data) {
+        return $this->db->insert('pengiriman', $data);
+    }
+
+    public function getSupplier() {
+        return $this->db->get('supplier')->result();
+    }
+
+    public function getBarang() {
+        return $this->db->get('barang')->result();
+    }
+}
+?>
 
