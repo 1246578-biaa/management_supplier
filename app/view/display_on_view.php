488e2b23ede6f943ab1fd867ecb58dc2fb62a48e
Amelia7-supermassive <cameliamel.76@gmail.com>
2025-10-28 00:15:10 +0700

    Update display_on_view.php

--- app/view/display_on_view.php
+++ app/view/display_on_view.php
@@ -1,1 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Edit Data Display On</title>
+</head>
+<body>
+<h2>Edit Data Display On</h2>
+<form method="POST" action="">
+    <label>ID Barang (tidak bisa diubah):</label><br>
+    <input type="number" name="id_barang" value="<?= $data['id_barang'] ?>" readonly><br><br>
 
+    <label>ID Etalase:</label><br>
+    <input type="number" name="id_etalase" value="<?= $data['id_etalase'] ?>" required><br><br>
+
+    <button type="submit">Update</button>
+</form>
+</body>
+</html>
