71ff79cef32c2e1a96035639a9dab42cdc975b49
Amelia7-supermassive <cameliamel.76@gmail.com>
2025-10-27 21:37:06 +0700

    Update etalase.php

--- app/model/etalase.php
+++ app/model/etalase.php
@@ -1,1 +1,18 @@
+<?php
+require_once 'app/config/database.php';
 
+class Etalase {
+    private $conn;
+
+    public function __construct() {
+        $database = new Database();
+        $this->conn = $database->conn;
+    }
+
+    public function tampilkanSemua() {
+        $query = "SELECT * FROM etalase";
+        $result = $this->conn->query($query);
+        return $result;
+    }
+}
+?>
