6c665410bc421aef6016cd5d696053cc5e5f1e98
rifqifirmansyah7 <rifqifirmansyah51@gmail.com>
2025-10-27 21:07:19 +0700

    Update suppliercontroller.php

--- app/controllers/suppliercontroller.php
+++ app/controllers/suppliercontroller.php
@@ -1,1 +1,11 @@
+<?php
+require_once 'app/model/Supplier.php';
 
+class SupplierController {
+    public function index() {
+        $supplier = new Supplier();
+        $data = $supplier->tampilkanSemua();
+        include 'app/view/supplier_view.php';
+    }
+}
+?>
