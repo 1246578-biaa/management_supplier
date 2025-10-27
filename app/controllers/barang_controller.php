40d46c6ecfa0f8fb6e64f4b213ac616f98934825
W.Oktavia <wahyuoktavia1176@gmail.com>
2025-10-27 22:17:41 +0700

    Update barang_controller.php

--- app/controllers/barang_controller.php
+++ app/controllers/barang_controller.php
@@ -1,1 +1,22 @@
+<?php
+include_once "./model/barang.php";
+
+class barang_controller {
+    private $model;
+
+    public function __construct($db) {
+        $this->model = new barang.php($db);
+    }
+
+    public function index() {
+        $data = $this->model->getAllBarang();
+        include "./view/barangView.php";
+    }
+
+    public function add($id_barang$nama_barang, $stok, $harga, $kondisi_barang) {
+        $this->model->addBarang($id_barang,$nama_barang, $stok, $harga, $kondisi_barang);
+        header("Location: landing.php"); // refresh halaman
+    }
+}
+?>
 
