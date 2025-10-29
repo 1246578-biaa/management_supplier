c4db1e13c13ad429d1b45c5a5fe904543f7354ff
Amelia7-supermassive <cameliamel.76@gmail.com>
2025-10-29 12:56:14 +0700

    Delete app/model/display_on.php

--- app/model/display_on.php
+++
@@ -1,9 +0,0 @@
-public function update() {
-    $query = "UPDATE display_on 
-              SET id_etalase = :id_etalase 
-              WHERE id_barang = :id_barang";
-    $stmt = $this->conn->prepare($query);
-    $stmt->bindParam(":id_etalase", $this->id_etalase);
-    $stmt->bindParam(":id_barang", $this->id_barang);
-    return $stmt->execute();
-}
