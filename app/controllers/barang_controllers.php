95f38ae082bd6c9430dd6247bdf8b9d6de89da82
rifqifirmansyah7 <rifqifirmansyah51@gmail.com>
2025-10-31 14:38:10 +0700

    Update barang_controllers.php

--- app/controllers/barang_controllers.php
+++ app/controllers/barang_controllers.php
@@ -1,1 +1,16 @@
+<?php
+require_once __DIR__ . '/../models/barang.php';
+
+class barang_controllers {
+    private $model;
+
+    public function __construct() {
+        $this->model = new barang();
+    }
+
+    public function tampilBarang() {
+        return $this->model->getAllBarang();
+    }
+}
+?>
 
