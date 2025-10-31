a6e9b48f74586fcea9627216f965422745185b38
rifqifirmansyah7 <rifqifirmansyah51@gmail.com>
2025-10-31 14:38:37 +0700

    Update barang.php

--- app/models/barang.php
+++ app/models/barang.php
@@ -1,1 +1,27 @@
+<?php
+require_once __DIR__ . '/../config/koneksi.php';
 
+class barang {
+    private $koneksi;
+
+    public function __construct() {
+        $this->koneksi = koneksiDatabase();
+    }
+
+    public function getAllBarang() {
+        $query = "SELECT * FROM barang";
+        $result = mysqli_query($this->koneksi, $query);
+
+        if (!$result) {
+            die("Query error: " . mysqli_error($this->koneksi));
+        }
+
+        $data = [];
+        while ($row = mysqli_fetch_assoc($result)) {
+            $data[] = $row;
+        }
+
+        return $data;
+    }
+}
+?>
