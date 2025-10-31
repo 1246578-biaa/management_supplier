d00c8ab3277b2dfcac0115a19a8479feb6b0195e
rifqifirmansyah7 <rifqifirmansyah51@gmail.com>
2025-10-31 14:37:48 +0700

    Update koneksi.php

--- app/config/koneksi.php
+++ app/config/koneksi.php
@@ -1,1 +1,12 @@
+<?php
+function koneksiDatabase() {
+    $koneksi = mysqli_connect("localhost", "root", "", "management_supplier3");
+
+    if (!$koneksi) {
+        die("Koneksi database gagal: " . mysqli_connect_error());
+    }
+
+    return $koneksi;
+}
+?>
 
