0b415d27fdcc84195a8ed54bd963b83024b004cc
Amelia7-supermassive <cameliamel.76@gmail.com>
2025-11-01 09:48:44 +0700

    Update supplier.php

--- app/models/supplier.php
+++ app/models/supplier.php
@@ -1,1 +1,16 @@
+<?php
+class Supplier {
+    private $conn;
+    private $table_name = "supplier";
 
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+
+    public function readAll() {
+        $query = "SELECT * FROM " . $this->table_name;
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        return $stmt;
+    }
+}
