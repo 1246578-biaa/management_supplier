1f9c8f3d87d4a142464c76177723675f8a9294b9
Amelia7-supermassive <cameliamel.76@gmail.com>
2025-11-01 09:49:54 +0700

    Update supplier_controllers.php

--- app/controllers/supplier_controllers.php
+++ app/controllers/supplier_controllers.php
@@ -1,1 +1,15 @@
+<?php
+require_once __DIR__ . '/../models/supplier.php';
+require_once __DIR__ . '/../config/database.php';
 
+class SupplierController {
+    public function index() {
+        $database = new Database();
+        $db = $database->getConnection();
+
+        $supplier = new Supplier($db);
+        $stmt = $supplier->readAll();
+
+        include __DIR__ . '/../views/supplier_views.php';
+    }
+}
