4a120fb9455dbda63449c7e97ab1847efbbea6cd
rifqifirmansyah7 <rifqifirmansyah51@gmail.com>
2025-11-02 16:12:24 +0700

    Add files via upload

---
+++ template/footer.php
@@ -0,0 +1,18 @@
+      </div> <!-- /#page-wrapper -->
+   </div> <!-- /#wrapper -->
+
+   <!-- JS -->
+   <script src="<?= $base_url ?>js/jquery-1.10.2.js"></script>
+   <script src="<?= $base_url ?>js/bootstrap.min.js"></script>
+   <script src="<?= $base_url ?>js/plugins/metisMenu/jquery.metisMenu.js"></script>
+   <script src="<?= $base_url ?>js/plugins/dataTables/jquery.dataTables.js"></script>
+   <script src="<?= $base_url ?>js/plugins/dataTables/dataTables.bootstrap.js"></script>
+   <script src="<?= $base_url ?>js/sb-admin.js"></script>
+
+   <script>
+      $(document).ready(function () {
+         $('.table').DataTable();
+      });
+   </script>
+</body>
+</html>
