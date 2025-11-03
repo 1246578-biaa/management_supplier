60017123f6c5190c8af002f026e1753e0cc2bfa6
rifqifirmansyah7 <rifqifirmansyah51@gmail.com>
2025-11-03 19:34:08 +0700

    Update supplier_views.php

--- app/views/supplier_views.php
+++ app/views/supplier_views.php
@@ -1,19 +1,45 @@
-<div class="container mt-4">
-    <h2>Data Supplier</h2>
-        <table border="1" cellpadding="8" cellspacing="0">
-            <tr>
-                <th>ID</th>
-                <th>Nama Supplier</th>
-                <th>Alamat</th>
-                <th>No Telepon</th>
-            </tr>
-            <?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) { ?>
-                <tr>
-                    <td><?= htmlspecialchars($row['id_supplier']); ?></td>
-                    <td><?= htmlspecialchars($row['nama_supplier']); ?></td>
-                    <td><?= htmlspecialchars($row['alamat']); ?></td>
-                    <td><?= htmlspecialchars($row['no_telp']); ?></td>
-                </tr>
-            <?php } ?>
-        </table>
+<?php include __DIR__ . '/../../template/header.php'; ?>
+
+<div class="row">
+    <div class="col-lg-12">
+        <h1 class="page-header">Master Supplier</h1>
+    </div>
 </div>
+
+<div class="row">
+    <div class="col-lg-12">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Data Supplier dari Database
+            </div>
+            <div class="panel-body">
+                <div class="table-responsive">
+                    <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Nama Supplier</th>
+                                <th>Alamat</th>
+                                <th>Telepon</th>
+                                <th>Tipe supplier</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) { ?>
+                                <tr>
+                                    <td><?= htmlspecialchars($row['id_supplier']); ?></td>
+                                    <td><?= htmlspecialchars($row['nama_supplier']); ?></td>
+                                    <td><?= htmlspecialchars($row['alamat']); ?></td>
+                                    <td><?= htmlspecialchars($row['no_telp']); ?></td>
+                                    <td><?= htmlspecialchars($row['tipe_supplier']); ?></td>
+                                </tr>
+                            <?php } ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include __DIR__ . '/../../template/footer.php'; ?>
