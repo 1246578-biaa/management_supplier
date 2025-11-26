bdecfccda58fbbefec43ac77faf9bb6644fcf347
W.Oktavia <wahyuoktavia1176@gmail.com>
2025-11-27 00:32:34 +0700

    Create footer.php

---
+++ management_supplier/app/views/template/footer.php
@@ -0,0 +1,18 @@
+        </div> <!-- /#page-wrapper -->
+    </div> <!-- /#wrapper -->
+
+    <!-- Core Scripts -->
+    <script src="admin/js/jquery-1.10.2.js"></script>
+    <script src="admin/js/bootstrap.min.js"></script>
+    <script src="admin/js/plugins/metisMenu/jquery.metisMenu.js"></script>
+    <script src="admin/js/plugins/dataTables/jquery.dataTables.js"></script>
+    <script src="admin/js/plugins/dataTables/dataTables.bootstrap.js"></script>
+    <script src="admin/js/sb-admin.js"></script>
+
+    <script>
+        $(document).ready(function() {
+            $('#dataTables-example').dataTable();
+        });
+    </script>
+</body>
+</html>
