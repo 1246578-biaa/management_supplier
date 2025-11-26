e05c444c1acd047eeed62ce43b9866b5a03a17a1
rifqifirmansyah7 <rifqifirmansyah51@gmail.com>
2025-11-26 22:52:06 +0700

    Update index.php

--- management_supplier/landing/index.php
+++ management_supplier/landing/index.php
@@ -1,1 +1,222 @@
+<!DOCTYPE html>
+<html lang="id">
 
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;600;700&display=swap" rel="stylesheet">
+
+    <title>Project UMKM</title>
+    <!--
+        
+    TemplateMo 558 Klassy Cafe
+
+    https://templatemo.com/tm-558-klassy-cafe
+
+    -->
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" type="text/css" href="/management_supplier/landing/assets/css/bootstrap.min.css">
+    
+    <link rel="stylesheet" type="text/css" href="/management_supplier/landing/assets/css/font-awesome.css">
+
+    <link rel="stylesheet" href="/management_supplier/landing/assets/css/flex-slider.css">
+    <link rel="stylesheet" href="/management_supplier/landing/assets/css/templatemo-klassy-cafe.css">
+
+    <link rel="stylesheet" href="/management_supplier/landing/assets/css/owl-carousel.css">
+
+    <link rel="stylesheet" href="/management_supplier/landing/assets/css/lightbox.css">
+
+    </head>
+    
+    <body>
+    
+    <!-- ***** Preloader Start ***** -->
+    <div id="preloader">
+        <div class="jumper">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>  
+    <!-- ***** Preloader End ***** -->
+    
+    
+    <!-- ***** Header Area Start ***** -->
+    <header class="header-area header-sticky">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <nav class="main-nav">
+                        <!-- ***** Logo Start ***** -->
+                        <a href="/management_supplier/landing/index.html" class="logo">
+                            <img src="/management_supplier/landing/assets/images/logo-toko.png" align="">
+                        </a>
+                        <!-- ***** Logo End ***** -->
+                        <!-- ***** Menu Start ***** -->
+                        <ul class="nav">
+                            <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
+                            <li class="scroll-to-section"><a href="#about">About</a></li>
+                           	
+                            <!-- Login Admin (tampilan kanan atas) -->
+                            <li class="scroll-to-section">
+                                <a href="/management_supplier/index.php?controller=login&action=index" class="login-admin-btn">Login</a>
+                            </li>
+                        </ul>        
+                        <!-- <a class='menu-trigger'>
+                            <span>Menu</span>
+                        </a> -->
+                        <!-- ***** Menu End ***** -->
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- ***** Header Area End ***** -->
+
+    <!-- ***** Main Banner Area Start ***** -->
+    <div id="top">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="left-content">
+                        <div class="inner-content">
+                            <h4>EL-Fat FotoCoppy</h4>
+                            <!-- <h6>THE BEST EXPERIENCE</h6> -->
+                            <!-- <div class="main-white-button scroll-to-section">
+                                <a href="#reservation">Hubungi Kami</a>
+                            </div> -->
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-8">
+                    <div class="main-banner header-text">
+                        <div class="Modern-Slider">
+                          <!-- Item -->
+                          <div class="item">
+                            <div class="img-fill">
+                                <img src="/management_supplier/landing/assets/images/slide-foto-1.jpg" alt="">
+                            </div>
+                          </div>
+                          <!-- // Item -->
+                          <!-- Item -->
+                          <div class="item">
+                            <div class="img-fill">
+                                <img src="/management_supplier/landing/assets/images/slide-foto-2.jpg" alt="">
+                            </div>
+                          </div>
+                          <!-- // Item -->
+                          <!-- Item -->
+                          <div class="item">
+                            <div class="img-fill">
+                                <img src="/management_supplier/landing/assets/images/slide-foto-3.jpg" alt="">
+                            </div>
+                          </div>
+                          <!-- // Item -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ***** Main Banner Area End ***** -->
+
+    <!-- ***** About Area Starts ***** -->
+    <section class="section" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-xs-12">
+                    <div class="left-text-content">
+                        <div class="section-heading">
+                            <h6>About Us</h6>
+                            <h2>EL-FATH Foto Copy dan Penjilidan</h2>
+                        </div>
+                        <p>Sebuah usaha jasa percetakan kecil hingga menengah yang menyediakan layanan fotokopi, pengetikan, penjilidan, dan kebutuhan dokumen lainnya. Tempat ini biasanya menjadi tujuan masyarakat sekitar untuk memenuhi berbagai kebutuhan administrasi, baik untuk sekolah, perkuliahan, maupun keperluan kantor. <br><br>Dengan pelayanan yang cepat, ramah, dan harga terjangkau, toko ini berkomitmen memberikan kemudahan dalam pengolahan dan pencetakan dokumen sehari-hari.</p>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-xs-12">
+                    <div class="right-content">
+                        <div class="thumb">
+                            <!--<a rel="nofollow" href="http://youtube.com"><i class="fa fa-play"></i></a>-->
+                            <img src="/management_supplier/landing/assets/images/foto-toko.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ***** About Area Ends ***** -->
+    
+    <!-- ***** Footer Start ***** -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-xs-12">
+                    <div class="right-text-content">
+                        <ul class="social-icons">
+                            <li>
+                                <a href="https://wa.me/6281336583839" target="_blank">
+                                    <i class="fa fa-whatsapp"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="logo">
+                        <a href="/management_supplier/landing/index.php"><img src="/management_supplier/landing/assets/images/logo-toko-bawah.png" alt=""></a>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-xs-12">
+                    <div class="left-text-content">
+                        <p>&copy; <?= date('Y') ?> Management Supplier.
+                        
+                        <br>All rights reserved.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="/management_supplier/landing/assets/js/jquery-2.1.0.min.js"></script>
+
+    <!-- Bootstrap -->
+    <script src="/management_supplier/landing/assets/js/popper.js"></script>
+    <script src="/management_supplier/landing/assets/js/bootstrap.min.js"></script>
+    <!-- Plugins -->
+    <script src="/management_supplier/landing/assets/js/owl-carousel.js"></script>
+    <script src="/management_supplier/landing/assets/js/accordions.js"></script>
+    <script src="/management_supplier/landing/assets/js/datepicker.js"></script>
+    <script src="/management_supplier/landing/assets/js/scrollreveal.min.js"></script>
+    <script src="/management_supplier/landing/assets/js/waypoints.min.js"></script>
+    <script src="/management_supplier/landing/assets/js/jquery.counterup.min.js"></script>
+    <script src="/management_supplier/landing/assets/js/imgfix.min.js"></script> 
+    <script src="/management_supplier/landing/assets/js/slick.js"></script> 
+    <script src="/management_supplier/landing/assets/js/lightbox.js"></script> 
+    <script src="/management_supplier/landing/assets/js/isotope.js"></script> 
+    
+    <!-- Global Init -->
+    <script src="/management_supplier/landing/assets/js/custom.js"></script>
+    <script>
+
+        $(function() {
+            var selectedClass = "";
+            $("p").click(function(){
+            selectedClass = $(this).attr("data-rel");
+            $("#portfolio").fadeTo(50, 0.1);
+                $("#portfolio div").not("."+selectedClass).fadeOut();
+            setTimeout(function() {
+              $("."+selectedClass).fadeIn();
+              $("#portfolio").fadeTo(50, 1);
+            }, 500);
+                
+            });
+        });
+
+    </script>
+  </body>
+</html>
