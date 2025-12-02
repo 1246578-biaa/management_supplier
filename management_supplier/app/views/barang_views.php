4293aa7d02f75f6f93ed5df166df41255cf25b13
Amelia7-supermassive <cameliamel.76@gmail.com>
2025-12-02 23:49:54 +0700

    Update barang_views.php

--- management_supplier/app/views/barang_views.php
+++ management_supplier/app/views/barang_views.php
@@ -1,130 +1,131 @@
 <?php include __DIR__ . "/template/header.php"; ?>
 
 <div class="container-fluid mt-4">
-    <h3 class="mb-3">Barang</h3>
 
-    <div class="panel panel-primary">
-        <div class="panel-heading">
-            <strong>Tambah Barang</strong>
-        </div>
+<h3>Data Barang</h3>
 
-        <div class="panel-body">
-            <form action="?controller=barang&action=simpan" method="POST" enctype="multipart/form-data">
+<div class="panel panel-primary">
+    <div class="panel-heading">Tambah Barang</div>
+    <div class="panel-body">
 
-                <div class="row">
-                    <div class="col-md-3">
-                        <label>ID Barang</label>
-                        <input type="text" name="id_barang" class="form-control" value="<?= $idBaru ?>" readonly>
-                    </div>
+        <form action="?controller=barang&action=simpan" method="POST" enctype="multipart/form-data">
+            <div class="row">
 
-                    <div class="col-md-3">
-                        <label>Supplier</label>
-                        <select name="id_supplier" class="form-control" required>
-                            <option value="">-- Pilih Supplier --</option>
-                            <?php foreach ($suppliers as $s): ?>
-                                <option value="<?= $s['id_supplier'] ?>">
-                                    <?= $s['id_supplier'] . " - " . $s['nama_supplier'] ?>
-                                </option>
-                            <?php endforeach; ?>
-                        </select>
-                    </div>
-
-                    <div class="col-md-3">
-                        <label>Nama Barang</label>
-                        <input type="text" name="nama_barang" class="form-control" required>
-                    </div>
-
-                    <div class="col-md-3">
-                        <label>Stok</label>
-                        <input type="number" name="stok" class="form-control" min="1" required>
-                    </div>
-
-                    <div class="col-md-3">
-                        <label>Harga</label>
-                        <input type="number" name="harga" class="form-control" min="100" required>
-                    </div>
-
-                    <div class="col-md-3">
-                        <label>Upload Gambar</label>
-                        <input type="file" name="gambar" class="form-control">
-                    </div>
+                <div class="col-md-2">
+                    <label>ID</label>
+                    <input type="text" name="id_barang" class="form-control" value="<?= $idBaru ?>">
                 </div>
 
-                <br>
-                <button type="submit" class="btn btn-primary">
-                    <span class="glyphicon glyphicon-save"></span> Simpan Barang
-                </button>
-            </form>
-        </div>
+                <div class="col-md-2">
+                    <label>Supplier</label>
+                    <select name="id_supplier" class="form-control" required>
+                        <option value="">Pilih</option>
+                        <?php foreach($suppliers as $s): ?>
+                            <option value="<?= $s['id_supplier'] ?>"><?= $s['nama_supplier'] ?></option>
+                        <?php endforeach ?>
+                    </select>
+                </div>
+
+                <div class="col-md-3">
+                    <label>Nama Barang</label>
+                    <input type="text" name="nama_barang" class="form-control" required>
+                </div>
+
+                <div class="col-md-2">
+                    <label>Stok</label>
+                    <input type="number" name="stok" class="form-control" required>
+                </div>
+
+                <div class="col-md-2">
+                    <label>Harga</label>
+                    <input type="number" name="harga" class="form-control" required>
+                </div>
+
+                <div class="col-md-3 mt-2">
+                    <label>Gambar</label>
+                    <input type="file" name="gambar" class="form-control">
+                </div>
+
+                <div class="col-md-3 mt-2">
+                    <br>
+                    <button class="btn btn-primary">Simpan Barang</button>
+                </div>
+
+            </div>
+        </form>
     </div>
-
-   
-    <h3 class="mb-3">Daftar Barang</h3>
-
-    <div class="card p-3 shadow-sm mb-4">
-        <label>Cari Barang</label>
-        <input type="text" id="searchBarang" class="form-control" placeholder="Cari ID / Nama / Supplier...">
-    </div>
-
-    <div class="panel panel-default">
-        <div class="panel-heading"><strong>Daftar Barang</strong></div>
-        <div class="panel-body">
-
-            <table class="table table-bordered table-striped" id="barangTable">
-                <thead>
-                    <tr>
-                        <th width="80">ID</th>
-                        <th>Supplier</th>
-                        <th>Nama Barang</th>
-                        <th width="60">Stok</th>
-                        <th width="120">Harga</th>
-                        <th width="80">Gambar</th>
-                        <th width="150">Aksi</th>
-                    </tr>
-                </thead>
-
-                <tbody>
-                    <?php foreach ($barangList as $b): ?>
-                    <tr>
-                        <td><?= $b['id_barang'] ?></td>
-                        <td><?= $b['nama_supplier'] ?></td>
-                        <td><?= $b['nama_barang'] ?></td>
-                        <td><?= $b['stok'] ?></td>
-                        <td>Rp <?= number_format($b['harga']) ?></td>
-                        <td>
-                            <?php if ($b['Gambar']): ?>
-                                <img src="assets/images/<?= $b['Gambar'] ?>" width="60" class="img-thumbnail">
-                            <?php else: ?>
-                                <span class="text-muted">-</span>
-                            <?php endif; ?>
-                        </td>
-
-                        <td>
-                            <a href="?controller=barang&action=edit&id=<?= $b['id_barang'] ?>"
-                               class="btn btn-warning btn-sm">
-                               <span class="glyphicon glyphicon-pencil"></span> Edit
-                            </a>
-
-                            <a href="?controller=barang&action=hapus&id=<?= $b['id_barang'] ?>"
-                               onclick="return confirm('Hapus barang ini?')"
-                               class="btn btn-danger btn-sm">
-                               <span class="glyphicon glyphicon-trash"></span> Hapus
-                            </a>
-                        </td>
-                    </tr>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
-
-        </div>
-    </div>
-
 </div>
 
+
+<div class="panel panel-default">
+    <div class="panel-heading">Cari Barang</div>
+    <div class="panel-body">
+        <input type="text" id="searchBarang" class="form-control" placeholder="Cari barang...">
+    </div>
+</div>
+
+
+<div class="panel panel-default">
+    <div class="panel-heading">Daftar Barang</div>
+
+    <div class="panel-body">
+
+        <table class="table table-bordered table-striped" id="barangTable">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Supplier</th>
+                    <th>Nama</th>
+                    <th>Stok</th>
+                    <th>Harga</th>
+                    <th>Foto</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+
+            <tbody>
+            <?php foreach($barangList as $b): ?>
+                <tr>
+                    <td><?= $b['id_barang'] ?></td>
+                    <td><?= $b['nama_supplier'] ?></td>
+                    <td><?= $b['nama_barang'] ?></td>
+                    <td><?= $b['stok'] ?></td>
+                    <td>Rp <?= number_format($b['harga']) ?></td>
+                    <td>
+                        <?php if ($b['gambar']): ?>
+                            <img src="landing/assets/images/<?= $b['gambar'] ?>" width="60">
+                        <?php else: ?>
+                            -
+                        <?php endif ?>
+                    </td>
+                    <td>
+                        <a href="?controller=barang&action=edit&id=<?= $b['id_barang'] ?>" class="btn btn-warning btn-sm">Edit</a>
+                        <a onclick="return confirm('Hapus?')" href="?controller=barang&action=delete&id=<?= $b['id_barang'] ?>" class="btn btn-danger btn-sm">Hapus</a>
+                    </td>
+                </tr>
+            <?php endforeach ?>
+            </tbody>
+        </table>
+
+        <!-- PAGINATION SLIDE -->
+        <center>
+        <ul class="pagination">
+        <?php for ($i=1; $i <= $totalPage; $i++): ?>
+            <li class="<?= ($i==$page?'active':'') ?>">
+                <a href="?controller=barang&action=index&page=<?= $i ?>"><?= $i ?></a>
+            </li>
+        <?php endfor ?>
+        </ul>
+        </center>
+
+    </div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script>
-$("#searchBarang").on("keyup", function() {
+$("#searchBarang").on("keyup", function () {
     var value = $(this).val().toLowerCase();
-    $("#barangTable tbody tr").filter(function() {
+    $("#barangTable tbody tr").filter(function () {
         $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
     });
 });
