62623ad900ae297d50fe63369f1ba6f3a93ad580
W.Oktavia <wahyuoktavia1176@gmail.com>
2025-12-08 21:19:38 +0700

    Add files via upload

---
+++ app/views/template/footer.php
@@ -0,0 +1,20 @@
+    </div> <!-- /#page-wrapper -->
+</div> <!-- /#wrapper -->
+
+<!-- Core Scripts - SB Admin ORIGINAL -->
+<script src="admin/js/jquery-1.10.2.js"></script>
+<script src="admin/js/bootstrap.min.js"></script>
+<script src="admin/js/plugins/metisMenu/jquery.metisMenu.js"></script>
+<script src="admin/js/plugins/dataTables/jquery.dataTables.js"></script>
+<script src="admin/js/plugins/dataTables/dataTables.bootstrap.js"></script>
+<script src="admin/js/sb-admin.js"></script>
+
+<script>
+$(document).ready(function() {
+    $('#dataTables-example').dataTable(); 
+});
+</script>
+
+</body>
+</html>
+    
